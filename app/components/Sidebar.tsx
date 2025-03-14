3c9ee091073087b375b8e438a7c0b762a4e5f612
Muhammad Fahad <muhfahad960@gmail.com>
2025-03-14 13:01:37 +0500

    working on sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from "next/image";
+ 
 export default function Sidebar() {
 
   const handleClick = () => {
@@ -11,6 +13,10 @@ export default function Sidebar() {
       <div className="flex items-center justify-center h-16">
         <h1 className="text-xl font-bold">top</h1>
       </div>
+      <button type="submit" className="flex new-chat-button mx-4 px-2 py-2 gap-2 text-center">
+        <Image className="invert" src="/new-chat.png" alt="new-chat-logo" width={20} height={20}></Image>
+        New chat
+      </button>
       <div className="flex flex-col flex-grow p-4">
         <p className="chat-day">Today</p>
         <button onClick={handleClick} className="chat-button">Hi?</button>
--- app/page.tsx
+++ app/page.tsx
@@ -34,14 +34,14 @@ export default function Home() {
       </div>
       <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-3xl mx-auto mb-2 px-4">   
         <div className="relative w-full flex flex-col">
-          <textarea value={message} onChange={(e) => setMessage(e.target.value)} className="flex block w-full rounded-lg textarea px-3 py-2 text-base" placeholder="Ask anything" />
+          <textarea value={message} onChange={(e) => setMessage(e.target.value)} className="flex block w-full rounded-lg textarea px-3 py-3 text-base" placeholder="Ask anything" />
           {/* <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M7 16c-.595 0-1.077-.462-1.077-1.032V1.032C5.923.462 6.405 0 7 0s1.077.462 1.077 1.032v13.936C8.077 15.538 7.595 16 7 16z" fill="currentColor"></path>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M.315 7.44a1.002 1.002 0 0 1 0-1.46L6.238.302a1.11 1.11 0 0 1 1.523 0c.421.403.421 1.057 0 1.46L1.838 7.44a1.11 1.11 0 0 1-1.523 0z" fill="currentColor"></path>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M13.685 7.44a1.11 1.11 0 0 1-1.523 0L6.238 1.762a1.002 1.002 0 0 1 0-1.46 1.11 1.11 0 0 1 1.523 0l5.924 5.678c.42.403.42 1.056 0 1.46z" fill="currentColor"></path>
           </svg> */}
           <button disabled={message.length === 0} type="submit" className="button inline-flex items-center justify-center absolute end-2.5 bottom-2.5 px-4 py-2 text-center rounded-full text-sm">Go</button>
-        </div>  
+        </div>
       </form>
       <div className="bottom-line">
         <p className="">AI-generated, for reference only</p>
